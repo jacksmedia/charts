5b6a20cfc582bf19afcc5282b715bee624e34cbb
jacksmedia <alex@j4cks.com>
2020-12-11 10:01:07 -0800

    :construction: componentized header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Headline from './headline.js';
 import PieChartDemo from './PieChartDemo.js';
-import logo from './logo.svg';
 
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
@@ -11,20 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>
-            COVID-19 by Select US States
-          </h1>
-          <h3>
-            Data via
-            <a href="https://api.covidtracking.com/" 
-            target="_blank"
-            rel="noopener noreferrer">
-              &nbsp;The COVID Tracking Project at <em>The Atlantic</em>
-            </a>
-          </h3>
-        </header>
+        <Headline />
         <PieChartDemo />
         <h3>
           Check back soon for more infographics & stay safe this winter!
